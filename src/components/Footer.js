ce526dcd1ff1bffff262d7f765dd3f39ae19f70c
Loka Kanarp <lokakanarp@gmail.com>
2018-06-01 11:03:34 +0200

    made everything smaller

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer (props) {
 	return (
 		<div className="footer">
-			<p>Pet the dog was created by Loka Kanarp as a school assignment at Medieinstitutet 2018.</p>
+			<p>Pet the dog was created by Loka Kanarp as a school assignment at Medieinstitutet in 2018.<br /><a href="https://github.com/lokakanarp/pet_the_dog">Github repo.</a></p>
 		</div>
 	);
 }
